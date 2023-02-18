0050e0f4e22e8de45b352d9beb36b217b80c0753
Limark Dcunha <83493294+limarkdcunha@users.noreply.github.com>
2023-02-18 23:17:43 +0530

    Transcripts chunks

--- src/pages/transcript.jsx
+++ src/pages/transcript.jsx
@@ -4,18 +4,29 @@ import axios from "axios";
 import dataContext from "@/hooks/DataContext/dataContext";
 import authContext from "@/hooks/AuthContext/authContext";
 import { Container, Tabs, Tab } from "react-bootstrap";
+import TransHistory from "@/components/transcript/TransHistory";
 
 const transcript = () => {
+  const { user } = useContext(authContext);
+  const { sendTranscripts } = useContext(dataContext);
+  const [link, setLink] = useState("");
+  const [subject, setSubject] = useState("");
+  const [chapter, setChapter] = useState("");
+  const [yid, setYid] = useState("");
+  const initialSubs = ["Psychology", "Geography", "History"];
+  const handleSubmit = () => {
+    setYid(link.split("=")[1]);
+    let formData = {
+      user_id: user ? user.user_id : "test_user_1",
+      video_url: link,
+      chapter_name: chapter,
+      subject_name: subject,
+    };
+    sendTranscripts(formData);
+  };
   return (
-    <div className="mt-2">
-      <Tabs defaultActiveKey="tab1" id="uncontrolled-tab-example" className="mb-3" fill>
-        <Tab eventKey="tab1" title="Study Transcripts">
-          <p>Tab1</p>
-        </Tab>
-        <Tab eventKey="tab2" title="Create Transcripts">
-          Tab2
-        </Tab>
-      </Tabs>
+    <div>
+      <TransHistory />
     </div>
   );
 };
