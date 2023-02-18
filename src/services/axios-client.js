a993620dbc73de78e6a9b0483db051f0c9faa908
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2023-02-19 00:01:13 +0530

    Transcript issue

--- src/services/axios-client.js
+++ src/services/axios-client.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const SERVER_BASE_URL = "";
+const SERVER_BASE_URL = "https://j0sal-organic-fishstick-g7gxj7v7x643wjpq-5000.preview.app.github.dev/";
 
 const LOCAL_SERVER_URL = "http://127.0.0.1:5000/";
 
 const axiosClient = axios.create({
-  baseURL: LOCAL_SERVER_URL,
+  baseURL: SERVER_BASE_URL,
 });
 
 export default axiosClient;
