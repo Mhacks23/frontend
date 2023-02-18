b8b2406c60b381395d75c253788fafd8cbb316fd
Limark Dcunha <83493294+limarkdcunha@users.noreply.github.com>
2023-02-18 22:46:30 +0530

    added send transcripts

--- src/hooks/DataContext/DataState.jsx
+++ src/hooks/DataContext/DataState.jsx
@@ -3,32 +3,43 @@ import axiosClient from "./../../services/axios-client";
 import DataContext from "./dataContext";
 
 function DataState({ children }) {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    const getData = async () => {
-        await axiosClient
-            .get("data")
-            .then(function (response) {
-                const res = response.data;
-                setData(res);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
+  const getData = async () => {
+    await axiosClient
+      .get("data")
+      .then(function (response) {
+        const res = response.data;
+        setData(res);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
+  const sendTranscripts = async (data) => {
+    await axiosClient
+      .post("create_nodes", data)
+      .then(function (response) {
+        const res = response.data;
+        setData(res);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-
-    return (
-        <DataContext.Provider
-            value={{
-                data,
-                getData
-            }}
-        >
-            {children}
-        </DataContext.Provider>
-    );
+  return (
+    <DataContext.Provider
+      value={{
+        data,
+        getData,
+        sendTranscripts,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
 }
 
 export default DataState;
