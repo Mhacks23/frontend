a5fb0aa4208f5282a72af2fa3ec9b029a1ffc5f9
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2023-02-18 21:17:34 +0530

    Updated layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import Layout from "../components/common/Layout";
+import AuthState from "../hooks/AuthContext/AuthState";
+import DataState from "../hooks/DataContext/DataState";
+import "../styles/globals.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import Head from "next/head";
+import React from "react";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <title>StudyPat</title>
+        <meta name="description" content="StudyPat by TechBytes" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <AuthState>
+        <DataState>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+          <ToastContainer />
+        </DataState>
+      </AuthState>
+    </React.Fragment>
+  );
 }
 
 export default MyApp
