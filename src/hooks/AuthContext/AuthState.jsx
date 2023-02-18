e9ffbdddf93f6f3f1978ba28b1d7c525445bb7a5
Limark Dcunha <83493294+limarkdcunha@users.noreply.github.com>
2023-02-18 22:43:39 +0530

    bug fixes

--- src/hooks/AuthContext/AuthState.jsx
+++ src/hooks/AuthContext/AuthState.jsx
@@ -7,74 +7,74 @@ import React from "react";
 import { alertBox } from "../../utils";
 
 function AuthState({ children }) {
-    const [user, setUser] = useState(null);
-    // const router = useRouter();
-    const cookies = new Cookies();
+  const [user, setUser] = useState(null);
+  // const router = useRouter();
+  const cookies = new Cookies();
 
-    useEffect(() => {
-        const mUser = cookies.get("user");
-        if (!mUser) {
-            setUser(null);
-        } else {
-            setUser(mUser);
-        }
-    }, []);
+  useEffect(() => {
+    const mUser = cookies.get("user");
+    if (!mUser) {
+      setUser(null);
+    } else {
+      setUser(mUser);
+    }
+  }, []);
 
-    const login = async (formData) => {
-        await axiosClient
-            .post("login", formData)
-            .then(function (response) {
-                const res = response.data;
-                setUser(res);
-                cookies.set("user", JSON.stringify(res), {
-                    path: "/",
-                    maxAge: 60 * 60 * 24,
-                });
-                alertBox("Login Successful!", "success");
-            })
-            .catch(function (error) {
-                alertBox();
-                console.log(error);
-            });
-    };
+  const login = async (formData) => {
+    await axiosClient
+      .post("login", formData)
+      .then(function (response) {
+        const res = response.data;
+        // setUser(res);
+        // cookies.set("user", JSON.stringify(res), {
+        //     path: "/",
+        //     maxAge: 60 * 60 * 24,
+        // });
+        alertBox("Login Successful!", "success");
+      })
+      .catch(function (error) {
+        alertBox();
+        console.log(error);
+      });
+  };
 
-    const register = async (formData) => {
-        await axiosClient
-            .post("register", formData)
-            .then(function (response) {
-                const res = response.data;
-                setUser(res);
-                cookies.set("user", JSON.stringify(res), {
-                    path: "/",
-                    maxAge: 60 * 60 * 24,
-                });
-                alertBox("User Registration Complete!", "success");
-            })
-            .catch(function (error) {
-                console.log(error);
-                alertBox();
-            });
-    };
+  const register = async (formData) => {
+    await axiosClient
+      .post("register", formData)
+      .then(function (response) {
+        const res = response.data;
+        setUser(res);
+        cookies.set("user", JSON.stringify(res), {
+          path: "/",
+          maxAge: 60 * 60 * 24,
+        });
+        alertBox("User Registration Complete!", "success");
+      })
+      .catch(function (error) {
+        console.log(error);
+        alertBox();
+      });
+  };
 
-    const logout = () => {
-        cookies.remove("user");
-        setUser(null);
-        alertBox("Successfully Logged out!", "success");
-        Router.push("/");
-    };
+  const logout = () => {
+    cookies.remove("user");
+    setUser(null);
+    alertBox("Successfully Logged out!", "success");
+    Router.push("/");
+  };
 
-    return (
-        <AuthContext.Provider
-            value={{
-                user,
-                login,
-                register,
-                logout,
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        login,
+        register,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export default AuthState;
